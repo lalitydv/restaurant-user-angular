6138b4a4ff6f56affc7602ecc30040f3d5661ad4
CerbosysTechnologies <77067633+lalitydv@users.noreply.github.com>
2023-10-15 03:00:33 +0530

    chnages

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
-
+import { Component  } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent  {
+  tiles:any=[
+    {name:'Appetizers',img:'assets/Menu/Group 339040.png'},
+    {name:'Flame Grill Burgers',img:'assets/Menu/image 9.png'},
+    {name:'Veggie & Plant ',img:'assets/Menu/image 6.png'},
+    {name:'Sandwich ',img:'assets/Menu/image 9.png'},
+    {name:'Hot Chicken Entrees',img:'assets/Menu/image 12.png'},
+    {name:'Beef Entries',img:'assets/Menu/image 13.png'},
 
+  ]
+  Product:any=[
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/Rectangle 381.png'},
+
+  ]
+  Poster1:any=[
+    {img:'assets/Poster/poster1.png',img2:'assets/Poster/poster2.png'},
+    
+   
+ 
+
+  ]
+  PopularPro:any=[
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+    {name:'Serve with mayo and green chili sauce.',sortname:'Onion Rings',Price:'900',img:'assets/Product/pizza.png'},
+
+  ]
+
+ 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
       colors: {
         // Use "colors" instead of "color"
         Pink: "#FF3389",
+        LightPink: "#FDD4E5",
+        blue: "#2E108E",
+        Admin: "#F97316",
+        Admin: "#10B981",
+        Admin: "#F97316",
+        
       },
       backgroundImage: {
         footer: "url('/assets/Home/footerbg.svg')",
